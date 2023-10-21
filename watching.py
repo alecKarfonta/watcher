7779014023041f3d389c60ff4254bf665a7d4ce1
Alec Karfonta <aleckarfonta@gmail.com>
2023-10-21 18:53:06 -0400

    Multithreaded image capture and analysis

--- watching.py
+++ watching.py
@@ -58,7 +58,7 @@ def main():
     print ("main")
     watcher = Watcher(pre_init=True)
 
-    cam_1_string = 'rtsp://admin:@192.168.1.104'
+    cam_1_string = 'rtsp://admin:Cody3.1415@192.168.1.104'
     
     webcam_stream = WebcamStream(stream_id=cam_1_string) # 0 id for main camera
     webcam_stream.start()
@@ -69,9 +69,12 @@ def main():
     shape = frame.shape[:2]
     target_sizes = torch.tensor([shape])
 
-    out = cv2.VideoWriter('filename_1.avi',  
-                         cv2.VideoWriter_fourcc(*'MJPG'), 
-                         15, shape) 
+    out = cv2.VideoWriter(
+                'filename_1.avi',  
+                cv2.VideoWriter_fourcc(*'MJPG'), 
+                15, 
+                shape
+                ) 
 
 
     objects = watcher.get_objects(frame, target_sizes, is_profile=True)
@@ -141,6 +144,93 @@ def main():
     #    video_shower.frame = frame
     #    #cps.increment()
 
+#if __name__ == "__main__":
+#    main()
 
-if __name__ == "__main__":
-    main()
+import threading
+import time
+from queue import Queue
+import cv2
+
+frames = Queue(10)
+
+class ImageGrabber(threading.Thread):
+    def __init__(self, ID):
+        threading.Thread.__init__(self)
+        self.ID=ID
+        self.cam=cv2.VideoCapture(ID)
+
+    def run(self):
+        global frames
+        while True:
+            ret, frame = self.cam.read()
+            frames.put(frame)
+            time.sleep(0.03)
+
+
+class Main(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+
+        watcher = Watcher(pre_init=True)
+
+        out = cv2.VideoWriter(
+                    'filename_1.avi',  
+                    cv2.VideoWriter_fourcc(*'MJPG'), 
+                    15, 
+                    (1920, 1080)
+                    ) 
+        
+
+        target_sizes = torch.tensor([(1080, 1920)])
+        global frames
+        step = 0
+        frames_read = 0
+        objects = []
+        try:
+            while True: #and step < 100:
+                #if step % 100 == 0:
+                #    print(f"{step} frames")
+                step += 1
+                # If has frames to process
+                if(not frames.empty()):
+                    # Pop a frame from the queue
+                    self.Currframe = frames.get()
+
+                    if objects:
+                        self.Currframe = watcher.overlay_boxes(objects, self.Currframe, 1080, 1920)
+
+                    #print (f"Read frame: {self.Currframe[0] = }")
+                    out.write(self.Currframe) 
+
+                    #frames.task_done()
+                    frames_read += 1
+                    if frames_read % 100 == 0:
+                        print(f"{frames_read} frames in {step} steps")
+
+                    
+                    if frames_read % 10 == 0:
+                        objects = watcher.get_objects(self.Currframe, target_sizes, is_profile=True)
+                        if objects:
+                            print (f"found {len(objects)} objects")
+                        else:
+                            print ("No objects found")
+                if frames_read > 1000:
+                    print ("Done. Read 1000 frames.")
+                    break
+        except KeyboardInterrupt:
+            out.release()
+
+        out.release()
+cam_1_string = 'rtsp://admin:Cody3.1415@192.168.1.104'
+grabber = ImageGrabber(cam_1_string)
+main = Main()
+
+grabber.start()
+main.start()
+main.join()
+grabber.join()
+
+
