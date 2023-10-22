a5ae87d8b57c5756bc6831165c90ac7100a09679
Alec Karfonta <aleckarfonta@gmail.com>
2023-10-21 20:54:12 -0400

    clean up

--- watching.py
+++ watching.py
@@ -1,58 +1,46 @@
 import cv2
 import time
+import torch 
 from threading import Thread
 
 from watcher import Watcher
 
-
-# defining a helper class for implementing multi-threading 
-class WebcamStream :
-    # initialization method 
+class WebcamStream:
     def __init__(self, stream_id=0):
-        self.stream_id = stream_id # default is 0 for main camera 
+        self.stream_id = stream_id
         
-        # opening video capture stream 
-        self.vcap      = cv2.VideoCapture(self.stream_id)
-        if self.vcap.isOpened() is False :
-            print("[Exiting]: Error accessing webcam stream.")
+        self.cap = cv2.VideoCapture(self.stream_id)
+        if self.cap.isOpened() is False:
             exit(0)
-        fps_input_stream = int(self.vcap.get(5)) # hardware fps
-        print("FPS of input stream: {}".format(fps_input_stream))
-            
-        # reading a single frame from vcap stream for initializing 
-        self.grabbed , self.frame = self.vcap.read()
-        if self.grabbed is False :
-            print('[Exiting] No more frames to read')
+
+        self.grabbed , self.frame = self.cap.read()
+        if self.grabbed is False:
             exit(0)
-        # self.stopped is initialized to False 
+
         self.stopped = True
-        # thread instantiation  
         self.t = Thread(target=self.update, args=())
-        self.t.daemon = True # daemon threads run in background 
-        
-    # method to start thread 
+        self.t.daemon = True
+
     def start(self):
         self.stopped = False
         self.t.start()
-    # method passed to thread to read next available frame  
+
     def update(self):
         while True :
             if self.stopped is True :
                 break
-            self.grabbed , self.frame = self.vcap.read()
+            self.grabbed , self.frame = self.cap.read()
             if self.grabbed is False :
-                print('[Exiting] No more frames to read')
                 self.stopped = True
                 break 
-        self.vcap.release()
-    # method to return latest read frame 
+        self.cap.release()
+
     def read(self):
         return self.frame
-    # method to stop reading frames 
+
     def stop(self):
         self.stopped = True
 
-import torch 
 
 def main():
     print ("main")
@@ -124,28 +112,7 @@ def main():
     print("FPS: {} , Elapsed Time: {} ".format(fps, elapsed))
     # closing all windows 
     cv2.destroyAllWindows()
-    
-    #stream_1 = VideoStreamWidget(cam_1_string, "Cam1")
-    #stream_1.update()
-    #stream_1.show_frame()
 
-    #video_getter = VideoGet(cam_1_string).start()
-    #video_shower = VideoShow(video_getter.frame).start()
-    #cps = CountsPerSec().start()
-
-    #while True:
-    #    if video_getter.stopped or video_shower.stopped:
-    #        video_shower.stop()
-    #        video_getter.stop()
-    #        break
-
-    #    frame = video_getter.frame
-    #    #frame = putIterationsPerSec(frame, cps.countsPerSec())
-    #    video_shower.frame = frame
-    #    #cps.increment()
-
-#if __name__ == "__main__":
-#    main()
 
 import threading
 import time
